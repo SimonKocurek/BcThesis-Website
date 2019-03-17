fdf8fe7856e00180b2f91103ed40083ec2cd7712
Simon Kocurek <simon.kocurek@gmail.com>
2019-03-17 21:49:13 +0100

    Redirect page

--- src/demo/frontend/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-
-mkdir -p target
-
-browserify index.js | sed '/^$/d; s/^\s*//; s/\s*$//'  > target/bundle.js
-cat index.html | sed '/^$/d; s/^\s*//; s/\s*$//' > target/index.html
